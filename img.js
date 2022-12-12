0e664b8eb450dae71e88246624f7132f877ccc42
Riksi <orangeanush@gmail.com>
2022-12-12 11:47:15 +0000

    new imgs

--- img.js
+++ img.js
@@ -3,5 +3,6 @@ const imgs = {
     2: "bright_star.jpeg",
     5: "moving_waters.jpeg",
     7: "mask2.jpeg",
-    10: "fair_love.jpeg",
+    10: "fair_love_v2.jpeg",
+    12: "fair_love_awake_v2.jpeg",
 }
