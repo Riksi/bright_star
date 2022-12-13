ba425eb6256bdd170f71e0b9d6cbf21103d75a5a
Riksi <orangeanush@gmail.com>
2022-12-13 09:46:28 +0000

    diff img

--- img.js
+++ img.js
@@ -2,7 +2,7 @@ const imgs = {
     1: "bright_star2.jpeg",
     4: "hermit_v3.jpeg",
     5: "bright_star.jpeg",
-    6: "moving_waters_2.jpeg",
+    6: "waters2.jpeg",
     7: "mask2.jpeg",
     10: "fair_love_v3.jpeg",
     12: "fair_love_awake_v2.jpeg",
